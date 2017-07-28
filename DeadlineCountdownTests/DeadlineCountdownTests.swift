7929287135465f0e4af2cb6aa4394addb0b8ebd0
Nataly <ngranetchi@gmail.com>
2017-07-28 22:19:22 +0200

    Creating Tests for the DeadlineText Class

--- DeadlineCountdownTests/DeadlineCountdownTests.swift
+++ DeadlineCountdownTests/DeadlineCountdownTests.swift
@@ -11,6 +11,8 @@ import XCTest
 
 class DeadlineCountdownTests: XCTestCase {
     
+    var textUnderTest: DeadlineText!
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -18,19 +20,46 @@ class DeadlineCountdownTests: XCTestCase {
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        textUnderTest = nil
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testTodayIsDeadline() {
+        
+        textUnderTest = DeadlineText(years: 0, months: 0, days: 0)
+        XCTAssertEqual(textUnderTest.toString(), "Today is Deadline")
+
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testDisplayingAllParts() {
+        
+        textUnderTest = DeadlineText(years: 3, months: 5, days: 9)
+        XCTAssertEqual(textUnderTest.toString(), "3 Year(s) and 5 Month(s) and 9 Day(s)")
+    }
+    
+    func testDisplayingYearsAndDays() {
+        
+        textUnderTest = DeadlineText(years: 5, months: 0, days: 15)
+        XCTAssertEqual(textUnderTest.toString(), "5 Year(s) and 15 Day(s)")
+    }
+
+    func testDisplayingMonthsAndDays() {
+        
+        textUnderTest = DeadlineText(years: 0, months: 7, days: 3)
+        XCTAssertEqual(textUnderTest.toString(), "7 Month(s) and 3 Day(s)")
+    }
+    
+    func testDisplayingYears() {
+        
+        textUnderTest = DeadlineText(years: 25, months: 0, days: 0)
+        XCTAssertEqual(textUnderTest.toString(), "25 Year(s)")
+    }
+    
+    func testDisplayingDays() {
+        
+        textUnderTest = DeadlineText(years: 0, months: 0, days: 10)
+        XCTAssertEqual(textUnderTest.toString(), "10 Day(s)")
     }
     
 }
+
