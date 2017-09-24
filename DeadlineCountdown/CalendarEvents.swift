bcbd191cc6ca67209f4eb0bb232ec799cc210981
Nataly <ngranetchi@gmail.com>
2017-09-24 10:48:04 +0200

    Replacing if with guard

--- DeadlineCountdown/CalendarEvents.swift
+++ DeadlineCountdown/CalendarEvents.swift
@@ -36,54 +36,58 @@ class CalendarEvents {
     func addEventToCalendar(title: String, description: String?, startDate: Date, endDate: Date) -> String {
         let event = EKEvent(eventStore: eventStore)
         
-        if (EKEventStore.authorizationStatus(for: .event) == EKAuthorizationStatus.authorized) {
-            event.title = title
-            event.isAllDay = true
-            event.startDate = startDate
-            event.endDate = endDate
-            event.notes = description
-            event.calendar = eventStore.defaultCalendarForNewEvents
-            
-            do {
-                try eventStore.save(event, span: .thisEvent, commit: true)
-            } catch {
-                let alert = UIAlertController(title: "Event could not save", message: (error as NSError).localizedDescription, preferredStyle: .alert)
-                let OKAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-                alert.addAction(OKAction)
-            }
+        guard EKEventStore.authorizationStatus(for: .event) == EKAuthorizationStatus.authorized else {
+            return ""
+        }
+        
+        event.title = title
+        event.isAllDay = true
+        event.startDate = startDate
+        event.endDate = endDate
+        event.notes = description
+        event.calendar = eventStore.defaultCalendarForNewEvents
+        
+        do {
+            try eventStore.save(event, span: .thisEvent, commit: true)
+        } catch {
+            let alert = UIAlertController(title: "Event could not save", message: (error as NSError).localizedDescription, preferredStyle: .alert)
+            let OKAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(OKAction)
         }
         return event.eventIdentifier
     }
     
     func removeEvent(savedEventId: String) {
-        if (EKEventStore.authorizationStatus(for: .event) == EKAuthorizationStatus.authorized) {
-            if let event = eventStore.event(withIdentifier: savedEventId) {
-                
-                do{
-                    try eventStore.remove(event, span: EKSpan.thisEvent)
-                }
-                catch let error {
-                    print("Error removing events: ", error)
-                }
+        guard EKEventStore.authorizationStatus(for: .event) == EKAuthorizationStatus.authorized else {
+            return
+        }
+        
+        if let event = eventStore.event(withIdentifier: savedEventId) {
+            
+            do {
+                try eventStore.remove(event, span: EKSpan.thisEvent)
+            }
+            catch let error {
+                print("Error removing events: ", error)
             }
         }
     }
     
     func editEvent(savedEventId: String, title: String, startDate: Date, endDate: Date) {
-        if (EKEventStore.authorizationStatus(for: .event) == EKAuthorizationStatus.authorized) {
-            if let event = eventStore.event(withIdentifier: savedEventId) {
-                event.title = title
-                event.startDate = startDate
-                event.endDate = endDate
-                
-                do {
-                    try eventStore.save(event, span: .thisEvent, commit: true)
-                } catch {
-                    print("Error editing")
-                }
-
+        guard EKEventStore.authorizationStatus(for: .event) == EKAuthorizationStatus.authorized else {
+            return
+        }
+        
+        if let event = eventStore.event(withIdentifier: savedEventId) {
+            event.title = title
+            event.startDate = startDate
+            event.endDate = endDate
+            
+            do {
+                try eventStore.save(event, span: .thisEvent, commit: true)
+            } catch let error {
+                print("Error editing: ", error)
             }
         }
     }
-
 }
